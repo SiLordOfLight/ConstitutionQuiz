e44761615e68b88eac68fe98b8e1dfe4709a915e
SiLordOfLight <jatnerd@gmail.com>
2018-09-05 12:44:28 -0700

    Commit 3

--- general.js
+++ general.js
@@ -1,3 +1,13 @@
+function getQuestions() {
+  return ["Who has the power to impeach the President?"];
+}
+
+function getRightAnswer(q) {
+  var rightAnswers = ["Congress"];
+
+  return rightAnswers[q];
+}
+
 $("document").ready(function(){
   $(".answer_button").click(function (event) {
     event.target.style.backgroundColor = 'DodgerBlue';
@@ -6,4 +16,15 @@ $("document").ready(function(){
 
     $(selectOthers).css("background","white");
   });
+
+  $("#submitButton").hover(function (event) {
+    event.target.src = "blue_star.png";
+  }, function (event) {
+    event.target.src = "white_star.png";
+  });
+
+  $("#submitButton").click(function (event){
+    var answerButtons = document.getElementsByClassName('answer_button');
+
+  })
 });
